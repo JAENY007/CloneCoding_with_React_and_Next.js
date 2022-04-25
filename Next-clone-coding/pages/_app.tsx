53db10ef4065bf85273127a8d5ebed7aa6e0fd86
JAENY007 <wjswodus22@naver.com>
2022-04-25 17:56:08 +0900

    April 25th Final Update

--- Next-clone-coding/pages/_app.tsx
+++ Next-clone-coding/pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from '../app.feature/group/main/component/provider';
 import ScreenMain from '../app.feature/group/main/screen/ScreenMain';
-import ScreenBrowseAllPage from '../app.feature/group/browseAll/screen/screenBrowseAllPage';
-import ScreenFeed from '../app.feature/group/feed/screen/screenFeed';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
